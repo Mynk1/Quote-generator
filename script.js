754f04606f64293fd0e087fa8e1beed81bfcb52c
mynk1 <mynksharma20397@gmail.com>
2021-08-03 22:38:46 +0530

    Js twitter

--- script.js
+++ script.js
@@ -57,10 +57,21 @@ async function getQuote() {
     }
 }
 
+// Tweet Quote
+
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+
 
 //Event listeners
 
 newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 
 //on load
+
 getQuote();
